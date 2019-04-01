f1b40a7fb2f3b80fe7c206c1f9723004dec7680d
Monica Labbao y Aspiras <ma.labbao@gmail.com>
2019-04-01 20:14:55 +0800

    make a POST request with body

--- server/models/person.js
+++ server/models/person.js
@@ -15,9 +15,26 @@ module.exports = function(Person) {
     }
   
     Person.remoteMethod('greet', {
+        // /api/dev/monica
+        // /api/dev/eloy
+        // /api/dev/:name/status
         http: {path: '/say/:msg', verb: 'GET'},
         accepts: [
             {arg: 'msg', type: 'string', http: {source: 'path'}},
+        ],
+        returns: {root: true, type: 'Object'}
+    });
+
+    Person.passTheMessage = (msgBody) => {
+        return Promise.resolve({
+            message: `Hi ${msgBody.to} from ${msgBody.from}`,
+        });
+    };
+
+    Person.remoteMethod('passTheMessage', {
+        http: {path: '/say', verb: 'POST'},
+        accepts: [
+            {arg: 'msgBody', type: 'Object', http: {source: 'body'}},
         ],
         returns: {root: true, type: 'Object'}
     });
